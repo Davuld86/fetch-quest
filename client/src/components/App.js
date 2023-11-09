a89f2b2d8385515d421745c04b4c5cd2a0c0c7c8
David <DavidFriend86@hotmail.com>
2023-11-08 18:24:11 -0600

    something was wrong with react, fixed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
 import {BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Home from "./Home";
 function App() {
   return(
-    <h1>Project Client</h1>;
     <BrowserRouter>
-    <Routes>
-    <Route path='/' /> 
-    
-    </Routes>
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+      </Routes>
     </BrowserRouter>
-  ) 
+  )
 }
 
 export default App;
