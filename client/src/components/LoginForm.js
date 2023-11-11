4804942ae00f82459ecf32df29090a75bf2e14c9
David <DavidFriend86@hotmail.com>
2023-11-10 21:07:56 -0600

    formik on login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field} from 'formik';
+import { loginSchema } from '../schemas';
+
 export default function LoginForm({toggleLoginForm, handleLogin}) {
   const [showPassword, setShowPassword] = useState(false)
   const validate = values => {
@@ -17,22 +19,28 @@ export default function LoginForm({toggleLoginForm, handleLogin}) {
         <button onClick={()=>toggleLoginForm(false)}>X</button>
         <Formik
          initialValues={{username: '', password:''}}
-         onSubmit={async(values)=> {await new Promise((r) => setTimeout(r,500));
+         validationSchema={loginSchema}
+         onSubmit={async(values, actions)=> {await new Promise((r) => setTimeout(r,500));
         handleLogin(values);
-        values= {username: '', password:''}
+        actions.resetForm();
         }}>
+          {({errors, isSubmitting, touched}) =>(
           <Form>
             <label htmlFor='username'>Username:</label>
             <Field id= 'username' name='username' />
+            {errors.username && touched.username? (<p>{errors.username}</p>):null}
+
             <br/>
             <label htmlFor='password'>Password:</label>
             <Field id='password' name='password' type={showPassword? 'text': 'password'}/>
+            {errors.password && touched.password ? (<p>{errors.password}</p>):null}
             <br/>
             <label htmlFor='checkbox'>Show Password</label>
             <input type='checkbox' onChange={()=> setShowPassword((prev)=> (!prev))}/>
             <br/>
-            <button type='submit'>Login</button>
+            <button type='submit' disabled ={isSubmitting||!errors}>Login</button>
           </Form>
+          )}
         </Formik>
       </div>
     )
